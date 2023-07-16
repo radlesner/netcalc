8ef4199ba6b80511b8c6596559ff6850bdbe25ae
radlesner <radek3301@linux.pl>
2023-07-16 21:37:19 +0200

    Added basic implementation.

--- src/headers/gtk.h
+++ src/headers/gtk.h
@@ -1,5 +1,8 @@
+#include <glib.h>
 #include <gtk/gtk.h>
 
 static void on_window_closed();
-void calculate_button_clicked();
+void calculate_button_clicked(GtkWidget *widget, gpointer data);
 void gtkWindowInit(int argc, char *argv[]);
+void gtk_entry_get_ip_address(GtkEntry *entry, gpointer user_data);
+void gtk_entry_get_subnet_mask(GtkEntry *entry, gpointer user_data);
