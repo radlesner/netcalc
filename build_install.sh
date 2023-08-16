d5b0451cdac447c4ab854f1facc529cfddad3128
radlesner <radek3301@linux.pl>
2023-08-16 23:09:07 +0200

    Update build script.

--- build_install.sh
+++ build_install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-read -p ">> Install font for project? [n/Y] " font
-if [ "$font" = "Y" ] || [ "$font" = "y" ] ; then
-    if [ -e "/usr/share/fonts/adobe-source-code-pro/" ] ; then
-        echo "-- Source Code Pro font are installed."
-    else
+if [ -e "/usr/share/fonts/adobe-source-code-pro/" ] ; then
+    echo "-- Source Code Pro font are installed."
+else
+    read -p ">> Install font for project? [n/Y] " font
+    if [ "$font" = "Y" ] || [ "$font" = "y" ] ; then
         printf "-- Installing font... ."
-        sudo cp -n -r ./resources/adobe-source-code-pro/ /usr/share/fonts
+        sudo cp -n -r ./resources/adobe-source-code-pro/ /usr/share/fonts > /dev/null
         printf "Done\n"
 
         printf "-- Refresh fonts... ."
-        sudo fc-cache -fv
+        sudo fc-cache -fv > /dev/null
         printf "Done\n"
     fi
 fi
