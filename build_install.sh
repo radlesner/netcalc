0e6ebf95af8181f1a64cdcb72d122db160a981f8
radlesner <radek3301@linux.pl>
2023-08-16 22:28:31 +0200

    Added font to resources

--- build_install.sh
+++ build_install.sh
@@ -1,10 +1,26 @@
-mkdir -p build
-cd build
+#!/bin/bash
 
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make
+read -p ">> Install font for project? [n/Y] " font
+if [ "$font" = "Y" ] || [ "$font" = "y" ] ; then
+    if [ -e "/usr/share/fonts/adobe-source-code-pro/" ] ; then
+        echo "-- Source Code Pro font are installed."
+    else
+        printf "-- Installing fonr... ."
+        sudo cp -n -r ./resources/adobe-source-code-pro/ /usr/share/fonts
+        printf "Done\n"
+    fi
+fi
 
-read -p "Install project? [N/Y]" install
-if [ "$install" = "Y" ] || [ "$install" = "y" ] ; then
-    sudo make install
+read -p ">> Build project? [n/Y] " build
+if [ "$build" = "Y" ] || [ "$build" = "y" ] ; then
+    mkdir -p build
+    cd build
+
+    cmake -DCMAKE_BUILD_TYPE=Release ..
+    make
+
+    read -p ">> Install project? [n/Y] " install
+    if [ "$install" = "Y" ] || [ "$install" = "y" ] ; then
+        sudo make install
+    fi
 fi
