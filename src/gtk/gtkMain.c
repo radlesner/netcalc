740c9af36b9fc07d3be668fc6f6ee77ffec4cc34
radlesner <radek3301@linux.pl>
2023-08-19 23:03:29 +0200

    Update window alignment

--- src/gtk/gtkMain.c
+++ src/gtk/gtkMain.c
@@ -201,7 +201,7 @@ void gtkWindowInit(int argc, char *argv[])
     // Program version output
     GtkWidget *fillBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
     gtk_box_pack_start(GTK_BOX(box1), fillBox, TRUE, TRUE, 0);
-    gtk_widget_set_size_request(GTK_WIDGET(fillBox), -1, 154);
+    gtk_widget_set_size_request(GTK_WIDGET(fillBox), -1, 171);
 
     printVersion(box1);
 
