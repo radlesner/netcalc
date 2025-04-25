f27e95eaa4ab24f64b22bb98ec248f03d73883ef
radlesner <radeklesner2000@gmail.com>
2025-04-25 18:23:04 +0200

    Update CMakeLists.txt - added font installation option

--- build_install.sh
+++ build_install.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 if [ -e "/usr/share/fonts/adobe-source-code-pro/" ] ; then
-    echo "-- Source Code Pro font are installed."
+    echo "[i] Source Code Pro font are installed."
 else
-    read -p ">> Install font for project? [n/Y] " font
+    read -p "[?] Install font for project? [n/Y] " font
     if [ "$font" = "Y" ] || [ "$font" = "y" ] ; then
-        printf "-- Installing font... ."
-        sudo cp -n -r ./resources/adobe-source-code-pro/ /usr/share/fonts > /dev/null
-        printf "Done\n"
+        echo "[i] Installing font... ."
+        sudo cp -n -r ./resources/adobe-source-code-pro/ /usr/share/fonts
 
-        printf "-- Refresh fonts... ."
-        sudo fc-cache -fv > /dev/null
-        printf "Done\n"
+        echo "[i] Refresh fonts... ."
+        sudo fc-cache -fv
     fi
 fi
 
-read -p ">> Build project? [n/Y] " build
+read -p "[?] Build project? [n/Y] " build
 if [ "$build" = "Y" ] || [ "$build" = "y" ] ; then
     mkdir -p build
     cd build
@@ -23,7 +21,7 @@ if [ "$build" = "Y" ] || [ "$build" = "y" ] ; then
     cmake -DCMAKE_BUILD_TYPE=Release ..
     make
 
-    read -p ">> Install project? [n/Y] " install
+    read -p "[?] Install project? [n/Y] " install
     if [ "$install" = "Y" ] || [ "$install" = "y" ] ; then
         sudo make install
     fi
